2a5480b57a2bb53b707a4daa5b030266ac0463eb
Ram Berrouet <clearlybetter@gmail.com>
2013-04-02 14:10:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lfly::Application.config.secret_token = '2f127a0a6d7b899fc550aa868850c0144e90fc2b007e05937affc0f53cdd7aab0671e05f2d6789d6f57a8a1d5f323e6bc00709f33d67286d360316b27bdb8312'
